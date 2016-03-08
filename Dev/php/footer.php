9fc48704f8a5194f1a62017ad82ff7dbc87b7810
Raxacoricofalapatorius <infosup@MarkIII.local>
2016-03-08 13:12:03 +0100

    Nav et footer studenthood

--- Dev/php/footer.php
+++ Dev/php/footer.php
@@ -1,42 +1,28 @@
 <div class="container">
 					<div class="row">
 						<div class="footer-ribbon">
-							<span>Get in Touch</span>
+							<span>Restez informés</span>
 						</div>
 						<div class="col-md-3">
 							<div class="newsletter">
-								<h4>Newsletter</h4>
-								<p>Keep up on our always evolving product features and technology. Enter your e-mail and subscribe to our newsletter.</p>
+								<h4>Partenaires</h4>
+								<p></p>
 			
-								<div class="alert alert-success hidden" id="newsletterSuccess">
-									<strong>Success!</strong> You've been added to our email list.
-								</div>
-			
-								<div class="alert alert-danger hidden" id="newsletterError"></div>
-			
-								<form id="newsletterForm" action="php/newsletter-subscribe.php" method="POST">
-									<div class="input-group">
-										<input class="form-control" placeholder="Email Address" name="newsletterEmail" id="newsletterEmail" type="text">
-										<span class="input-group-btn">
-											<button class="btn btn-default" type="submit">Go!</button>
-										</span>
-									</div>
-								</form>
 							</div>
 						</div>
 						<div class="col-md-3">
-							<h4>Latest Tweets</h4>
+							<h4>Derniers Twwets</h4>
 							<div id="tweet" class="twitter" data-plugin-tweets data-plugin-options='{"username": "", "count": 2}'>
 								<p>Please wait...</p>
 							</div>
 						</div>
 						<div class="col-md-4">
 							<div class="contact-details">
-								<h4>Contact Us</h4>
+								<h4>Contactez-nous</h4>
 								<ul class="contact">
-									<li><p><i class="fa fa-map-marker"></i> <strong>Address:</strong> 1234 Street Name, City Name, United States</p></li>
-									<li><p><i class="fa fa-phone"></i> <strong>Phone:</strong> (123) 456-789</p></li>
-									<li><p><i class="fa fa-envelope"></i> <strong>Email:</strong> <a href="mailto:mail@example.com">mail@example.com</a></p></li>
+									<li><p><i class="fa fa-map-marker"></i> <strong>Adresse:</strong> 139 rue des Pyrénées, 75020 Paris</p></li>
+									<li><p><i class="fa fa-phone"></i> <strong>Tel:</strong> 08 00 60 06 33</p></li>
+									<li><p><i class="fa fa-envelope"></i> <strong>Email:</strong> <a href="mailto:mail@example.com">contact@studenthood.fr</a></p></li>
 								</ul>
 							</div>
 						</div>
@@ -45,7 +31,7 @@
 							<ul class="social-icons">
 								<li class="social-icons-facebook"><a href="http://www.facebook.com/" target="_blank" title="Facebook"><i class="fa fa-facebook"></i></a></li>
 								<li class="social-icons-twitter"><a href="http://www.twitter.com/" target="_blank" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-								<li class="social-icons-linkedin"><a href="http://www.linkedin.com/" target="_blank" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+								<li class="social-icons-instagram"><a href="http://www.linkedin.com/" target="_blank" title="instagram"><i class="fa fa-instagram"></i></a></li>
 							</ul>
 						</div>
 					</div>
@@ -64,7 +50,6 @@
 							<div class="col-md-4">
 								<nav id="sub-menu">
 									<ul>
-										<li><a href="page-faq.html">FAQ's</a></li>
 										<li><a href="sitemap.html">Sitemap</a></li>
 										<li><a href="contact-us.html">Contact</a></li>
 									</ul>
