d59f55650fc924ce4f060e814edab0cd807f2dac
KarimGrandemange <karim.grandemange@ynov.com>
2016-03-10 09:00:24 +0100

    Update homepage-services.php

---
+++ Dev/php/homepage-services.php
@@ -0,0 +1,113 @@
+<head>
+    <link href="//www.gpaltemps.com/css/dist/eNpdjFEOgCAMQ-_iPxK_TOQ0k0zFAEM2Y7y9Gg0a_9q-ttw1bdtVllkHiDCi7omEJUN6VX1i86kMFAU2ZAq3Vo_598qBYvRopXCZ8JzOy4p5V6urg4uFsRPUo6ce_C9L4AVD4iuuzAGCwUYW.css" media="screen" rel="stylesheet" type="text/css">
+</head>
+
+<body class="page-mes one-column">
+    <div class="content">
+        <div class="wrapper container">   
+            <article id="col-center col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="row">
+                <div class="col-xs-12">
+                <div class="block popular">
+                    <div class="categories">
+                        <div class="category" data-url="/categories/mes-evenements-anniversaire-mariage-wedding-planner" aria-haspopup="true">
+                            <div class="icon-wrapper">
+                                <div class="icon-wrapper-table">
+                                    <div class="img/incone_evenement.jpg">
+                                    </div>
+                                </div>
+                            </div>
+                            <span class="name">ÉVÈNEMENTS</span>
+                        </div>
+                        <div class="category" data-url="/categories/aide-a-domicile-service-a-la-personne-pas-cher" aria-haspopup="true">
+                            <div class="icon-wrapper">
+                                <div class="icon-wrapper-table">
+                                    <div class="img/incone_cours.jpg">
+                                    </div>
+                                </div>
+                            </div>
+                            <span class="name">COURS</span>
+                        </div>
+                        <div class="category" data-url="/categories/mes-animaux" aria-haspopup="true">
+                            <div class="icon-wrapper">
+                                <div class="icon-wrapper-table">
+                                      <div class="img/incone_sports.jpg"> 
+                                      </div>
+                                </div>
+                            </div>
+                            <span class="name">SPORTS</span>
+                        </div>
+                        <div class="category" data-url="/categories/trouver-babysitter-nounou-a-domicile-garde-enfant" aria-haspopup="true">
+                            <div class="icon-wrapper">
+                                <div class="icon-wrapper-table">
+                                    <div class="img/incone_informatique.jpg">
+                                    </div>
+                                </div>
+                            </div>
+                            <span class="name">INFORMATIQUE</span>
+                        </div>
+                        <div class="category" data-url="/categories/ma-sante" aria-haspopup="true">
+                            <div class="icon-wrapper">
+                                <div class="icon-wrapper-table">
+                                    <div class="img/incone_cuisine.jpg">
+                                    </div>
+                                </div>
+                            </div>
+                            <span class="name">CUISINE</span>
+                        </div>
+                        <div class="category" data-url="/categories/coach-sportif-professeur-de-sport" aria-haspopup="true">
+                            <div class="icon-wrapper">
+                                <div class="icon-wrapper-table">
+                                    <div class="img/incone_bricolage.jpg">
+                                    </div>
+                                </div>
+                            </div>
+                            <span class="name">BRICOLAGE</span>
+                        </div>
+                        <div class="category" data-url="/categories/mon-look" aria-haspopup="true">
+                            <div class="icon-wrapper">
+                                <div class="icon-wrapper-table">
+                                    <div class="img/incone_jeux_video.jpg">
+                                    </div>
+                                </div>                            
+                            </div>
+                            <span class="name">JEUX VIDEO</span>
+                        </div>
+                        <div class="category" data-url="/categories/mes-voyages" aria-haspopup="true">
+                            <div class="icon-wrapper">
+                                <div class="icon-wrapper-table">
+                                    <div class="img/incone_electromenager.jpg">
+                                    </div>
+                                </div>
+                            </div>
+                            <span class="name">ELECTROMENAGER</span>
+                        </div>
+                        <div class="category" data-url="/categories/trouver-car-wash-domicile" aria-haspopup="true">
+                            <div class="icon-wrapper">
+                                <div class="icon-wrapper-table">
+                                    <div class="img/incone_vetements.jpg">
+                                    </div>
+                                </div>
+                            </div>
+                            <span class="name">VETEMENTS</span>
+                        </div>
+                        <div class="category" data-url="/categories/mes-formations" aria-haspopup="true">
+                            <div class="icon-wrapper">
+                                <div class="icon-wrapper-table">
+                                    <div class="img/incone_vehicules.jpg">
+                                </div>
+                            </div>
+                        </div>
+                        <span class="name">VEHICULES</span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <p>&nbsp;</p>
+                        <p>&nbsp;</p>        
+                    </div>
+                </div>
+            </article>
+        </div>
+    </div>
+</body>
