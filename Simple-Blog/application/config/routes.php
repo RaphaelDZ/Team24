aaf82a4eca7a023ff75d656ca837ca44095d7490
ilyan10 <ilyan.alouani@ynov.com>
2016-03-14 14:22:55 +0100

    back 14/03

---
+++ Simple-Blog/application/config/routes.php
@@ -0,0 +1,7 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+$route['default_controller'] = "blog";
+$route['404_override'] = '';
+
+
---
+++ Simple-Blog/application/views/v_contact.php
@@ -0,0 +1,16 @@
+<div id="site_content">
+    <div id="content">
+        <!-- insert the page content here -->
+        <h1>Contactez nous</h1>
+        <p>Voici un exemple:</p>
+        <form action="#" method="post">
+          <div class="form_settings">
+            <p><span>Nom</span><input class="contact" type="text" name="your_name" value="" /></p>
+            <p><span>Addresse mail</span><input class="contact" type="text" name="your_email" value="" /></p>
+            <p><span>Message</span><textarea class="contact textarea" rows="8" cols="50" name="your_enquiry"></textarea></p>
+            <p style="padding-top: 15px"><span>&nbsp;</span><input class="submit" type="submit" name="contact_submitted" value="submit" /></p>
+          </div>
+        </form>
+        
+    </div>
+</div>
