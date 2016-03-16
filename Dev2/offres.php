adf0bdce27c0d06b84b1297db6bd68996caac6ef
JuanSepu <juan.sepulvedahenao@ynov.com>
2016-03-16 13:48:59 +0100

    Ajout des prix

--- Dev2/offres.php
+++ Dev2/offres.php
@@ -18,27 +18,27 @@
 			<tbody>
 				<tr class="cellule">
 					<td class="cellule_rouge" style="border-radius: 10px 0 0 0;">Voir les annonces</td>
-					<td class="cellule_interieur_rouge_clair"><img class="img" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
-					<td class="cellule_interieur_rouge_clair"><img class="img" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
-					<td class="cellule_interieur_rouge_clair"><img class="img" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
+					<td class="cellule_interieur_rouge_clair"><img class="centrage" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
+					<td class="cellule_interieur_rouge_clair"><img class="centrage" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
+					<td class="cellule_interieur_rouge_clair"><img class="centrage" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
 				</tr>
 				<tr class="cellule">
 					<td class="cellule_rouge_clair">Poster des annonces</td>
-					<td class="cellule_interieur_rouge"><img class="img" src="img/icone_non.png" width="26" height="25" alt="Non"/></td>
-					<td class="cellule_interieur_rouge"><img class="img" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
-					<td class="cellule_interieur_rouge"><img class="img" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
+					<td class="cellule_interieur_rouge"><img class="centrage" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
+					<td class="cellule_interieur_rouge"><img class="centrage" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
+					<td class="cellule_interieur_rouge"><img class="centrage" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
 				</tr>
 				<tr class="cellule">
 					<td class="cellule_rouge">R&eacute;pondre a une annonce</td>
-					<td class="cellule_interieur_rouge_clair"><img class="img" src="img/icone_non.png" width="26" height="25" alt="Non"/></td>
-					<td class="cellule_interieur_rouge_clair"><img class="img" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
-					<td class="cellule_interieur_rouge_clair"><img class="img" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
+					<td class="cellule_interieur_rouge_clair"><img class="centrage" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
+					<td class="cellule_interieur_rouge_clair"><img class="centrage" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
+					<td class="cellule_interieur_rouge_clair"><img class="centrage" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
 				</tr>
 				<tr class="cellule">
 					<td class="cellule_rouge_clair">Noter une annonce</td>
-					<td class="cellule_interieur_rouge"><img class="img" src="img/icone_non.png" width="26" height="25" alt="Non"/></td>
-					<td class="cellule_interieur_rouge"><img class="img" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
-					<td class="cellule_interieur_rouge"><img class="img" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
+					<td class="cellule_interieur_rouge"><img class="centrage" src="img/icone_non.png" width="26" height="25" alt="Non"/></td>
+					<td class="cellule_interieur_rouge"><img class="centrage" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
+					<td class="cellule_interieur_rouge"><img class="centrage" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
 				</tr>
 				<tr class="cellule">
 					<td class="cellule_rouge">Limite pour poster des annonces</td>
@@ -47,10 +47,22 @@
 					<td class="cellule_interieur_rouge_clair police">Illimit&eacute;e</td>
 				</tr>
 				<tr class="cellule">
-					<td class="cellule_rouge_clair" style="border-radius: 0 0 0 10px;">Page de profil p&eacute;rsonnalis&eacute;e</td>
-					<td class="cellule_interieur_rouge"><img class="img" src="img/icone_non.png" width="26" height="25" alt="Non"/></td>
-					<td class="cellule_interieur_rouge"><img class="img" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
-					<td class="cellule_interieur_rouge" style="border-radius: 0 0 10px 0;"><img class="img" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
+					<td class="cellule_rouge_clair">Page de profil p&eacute;rsonnalis&eacute;e</td>
+					<td class="cellule_interieur_rouge"><img class="centrage" src="img/icone_non.png" width="26" height="25" alt="Non"/></td>
+					<td class="cellule_interieur_rouge"><img class="centrage" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
+					<td class="cellule_interieur_rouge"><img class="centrage" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
+				</tr>
+				<tr class="cellule">
+					<td class="cellule_rouge" style="border-radius: 0 0 0 10px;">Publicit&eacute;</td>
+					<td class="cellule_interieur_rouge_clair"><img class="centrage" src="img/icone_oui.png" width="26" height="25" alt="Oui"/></td>
+					<td class="cellule_interieur_rouge_clair"><img class="centrage" src="img/icone_non.png" width="26" height="25" alt="Non"/></td>
+					<td class="cellule_interieur_rouge_clair"><img class="centrage" src="img/icone_non.png" width="26" height="25" alt="Non"/></td>
+				</tr>
+				<tr class="cellule">
+					<td class="cellule_vide" style="border-radius: 0 0 0 10px;"></td>
+					<td class="cellule_interieur_rouge police" style="border-radius: 0 0 0 10px;"><a href="souscription.html" style="color: black">Gratuit</a></td>
+					<td class="cellule_interieur_rouge police"><a href="souscription.html" style="color: black">_€/mois</a></td>
+					<td class="cellule_interieur_rouge police" style="border-radius: 0 0 10px 0;"><a href="souscription.html" style="color: black">_€/an</a></td>
 				</tr>
 			</tbody>
 		</tr>
