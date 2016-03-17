8de431cf840dc4ff5e806a8adf055e622a40ff04
KarimGrandemange <karim.grandemange@ynov.com>
2016-03-17 16:14:46 +0100

    Update annonce.php

--- Dev2/php/annonce.php
+++ Dev2/php/annonce.php
@@ -14,66 +14,41 @@
         <?php include('public/php/nav.php'); ?>
     </header>
 
-    <table align="center" width="100%">
-        <td align="left">
-            <img src="http://4.bp.blogspot.com/-2Z6AlFgzp08/UheHBWUXi-I/AAAAAAAAADo/U7twcKl5fco/s1600/Image13.png"
-                 width="500px" height="1000px">
-        </td>
+    <div align="center">
+        
 
 
-        <td align="center">
-
-
-            <h1>ANNONCE</h1>
-
-
-            <div class="header-search hidden-xs">
-                <form id="searchForm" action="page-search-results.html" method="get">
-                    <div class="input-group">
-                        <input type="text" class="form-control" name="q" id="q" placeholder="Rechercher" required>
+        <div class="header-search hidden-xs">
+            <form id="searchForm" action="page-search-results.html" method="get">
+                <div class="input-group">
+                    <input type="text" class="form-control" name="q" id="q" placeholder="Rechercher" required>
                     <span class="input-group-btn">
                         <button class="btn btn-default" type="submit"><i class="fa fa-search">OK</i></button>
                     </span>
-                    </div>
-                </form>
-            </div>
+                </div>
+            </form>
+        </div>
 
 
-            <div>
-                <h2>annonce 1</h2>
-                <ul>
-                    <li>
-                        <h4>posteur</h4>
-                    </li>
-                    <li>
-                        <h4>Catégories</h4>
-                        <p>a définir (obligatoire)</p>
-                    </li>
-                    <li>
-                        <h4>Description</h4>
-                        <p>a définir (non obligatoire)</p>
-                    </li>
-                    <li>
-                        <h4>date</h4>
-                        <p>17/03/2016 11:00</p>
-                    </li>
-                </ul>
-            </div>
-
-            <input value="Poster une annonce" aria-label="Recherche Google" type="submit">
+        <div>
+            <h2>annonce 1</h2>
+            <ul>
+                <?php include('application/views/v_home_co.php'); ?>
 
 
-        </td>
-        <td align="right">
-            <img src="http://www.passeportetudiant71.fr/images/etudiant.jpg" width="500px" height="1000px">
-        </td>
-        </tr></tbody></table>
+            </ul>
+        </div>
+
+        <input value="Poster une annonce" aria-label="Recherche Google" type="submit">
+
+
+    </div>
     <footer id="footer">
         <?php include('public/php/footer.php'); ?>
     </footer>
 </div>
 
-<?php include('oublic/php/page-end.php'); ?>
+<?php include('public/php/page-end.php'); ?>
 
 
 </body>
