67a2b5ca3affe373b6c1504424299f80620239fc
AnthonyNabais <anthony.nabais@ynov.com>
2016-03-17 16:25:16 +0100

    Added files via upload

---
+++ Dev2/public/php/campus.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php include('php/homepage-head.php'); ?>
+    <link rel="stylesheet" type="text/css" href="css/offres.css"/>
+    <link rel="stylesheet" type="texte/css" href="css/campus.css"/>
+
+
+
+
+    <!-- Navbar -->
+<?php include('php/nav.php'); ?>
+</head>
+
+<body>
+
+<div class="yolo"></div>
+<!-- Tableau offres -->
+    <div align="center">
+        <I><U><B><h1>CAMPUS AFFILIÉS</h1></B></U></I>
+
+        <a href="https://www.ingesup.com/"><img alt="img_manga_semaine" src="https://www.ingesup.com/wp-content/themes/ingesup/images/logo.png"></a>
+                <ul>
+
+                        <h4>Description</h4>
+                    <p>Pellentesque habitant morbi tristique senectus</p>
+                    <h4>Ecole d'ingénieurie informatique</h4>
+                        <p>telephone:0 800 60 06 33</p>
+                </ul>
+                <a href="https://www.infosup.com/"><img alt="ingesup" src="https://www.infosup.com/wp-content/themes/infosup/images/logo.png"></a>
+                <ul>
+                        <h4>Description</h4>
+                    <p>Pellentesque habitant morbi tristique senectus</p>
+                    <h4>Ecole bts informatique</h4>
+                        <p>telephone:0800 600 633</p>
+                </ul>
+
+
+                <I><U><B><h1>CAMPUS NON AFFILIÉS</h1></B></U></I>
+                <h1>Lim'Art</h1></a>
+                <ul>
+                        <h4>Description</h4>
+                    <p>Pellentesque habitant morbi tristique senectus</p>
+                    <h4>Ecole d'art</h4>
+                </ul>
+                <h1>ESSCA</h1></a>
+                <ul>
+                        <h4>Description</h4>
+                    <p>Pellentesque habitant morbi tristique senectus</p>
+                        <h4>Ecole de management</h4>
+                </ul>
+
+    </div>
+
+
+
+<!-- Footer -->
+<?php include('php/footer.php'); ?>
+
+
+<?php include('php/end.php'); ?>
+</body>
+</html>
+
