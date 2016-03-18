853e31d3a36a36c91da9b91c97c840905e2a21a6
KarimGrandemange <karim.grandemange@ynov.com>
2016-03-18 09:18:33 +0100

    Update v_home_co.php

--- Dev2/application/views/v_home_co.php
+++ Dev2/application/views/v_home_co.php
@@ -1,26 +1,50 @@
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
 <div id="site_content">
-   
+
     <div id="content">
-            <form action="<?=  base_url()?>blog/new_post/" method="post">
-        <?php if($this->session->userdata('user_id') && $this->session->userdata('user_type') != 'user')
-        { ?>
-        <h2><a style="color: green" href="<?=  base_url()?>blog/new_post/"><span class="glyphicon glyphicon-pencil"></span> Create a new post</a></h2>
-        <?php } ?>
-    <!-- insert the page content here -->
-    <?php foreach($posts as $post)
-    { ?>
-    <h2><a style="color:red;" href="<?=  base_url()?>blog/post/<?=$post['post_id']?>"><?=$post['post_title'];?></a></h2>
-        <?php if($this->session->userdata('user_id') && $this->session->userdata('user_type') == 'admin')
-        { ?>
-        <p>
-            <a href="<?=  base_url()?>blog/editpost/<?=$post['post_id']?>"><span class="glyphicon glyphicon-edit" title="Edit post"></span></a> | 
-            <a href="<?=  base_url()?>blog/deletepost/<?=$post['post_id']?>"><span style="color:#f77;" class="glyphicon glyphicon-remove-circle" title="Delete post"></span></a>
-        </p>
-       <?php }?>
-        <p><?=  substr(strip_tags($post['post']), 0, 200).'...';?></p>
-        <p><a href="<?=  base_url()?>blog/post/<?=$post['post_id']?>">En savoir plus</a></p>
-    <?php
-    }?>
-    <?=$pages?>
+        <form action="<?= base_url() ?>blog/new_post/" method="post">
+            <?php if ($this->session->userdata('user_id') && $this->session->userdata('user_type') != 'user') { ?>
+                <h2><a style="color: green" href="<?= base_url() ?>blog/new_post/"><span
+                            class="glyphicon glyphicon-pencil"></span> Create a new post</a></h2>
+            <?php } ?>
+
+            <div class="header-search hidden-xs">
+                <form id="searchForm" action="page-search-results.html" method="get">
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="q" id="q" placeholder="Rechercher" required>
+                    <span class="input-group-btn">
+                        <button class="btn btn-default" type="submit"><i class="fa fa-search">OK</i></button>
+                    </span>
+                    </div>
+                </form>
+            </div>
+
+            <div align="center">
+            <!-- insert the page content here -->
+            <?php foreach ($posts as $post) { ?>
+                <h2><a style="color:red;"
+                       href="<?= base_url() ?>blog/post/<?= $post['post_id'] ?>"><?= $post['post_title']; ?></a></h2>
+                <?php if ($this->session->userdata('user_id') && $this->session->userdata('user_type') == 'admin') { ?>
+                    <p>
+                        <a href="<?= base_url() ?>blog/editpost/<?= $post['post_id'] ?>"><span
+                                class="glyphicon glyphicon-edit" title="Edit post"></span></a> |
+                        <a href="<?= base_url() ?>blog/deletepost/<?= $post['post_id'] ?>"><span style="color:#f77;"
+                                                                                                 class="glyphicon glyphicon-remove-circle"
+                                                                                                 title="Delete post"></span></a>
+                    </p>
+                <?php } ?>
+                <p><?= substr(strip_tags($post['post']), 0, 200) . '...'; ?></p>
+                <p><a href="<?= base_url() ?>blog/post/<?= $post['post_id'] ?>">En savoir plus</a></p>
+                <?php
+            } ?>
+            <?= $pages ?>
+            </div>
     </div>
 </div>
